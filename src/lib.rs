07802f6c9780e056c6fc5733b332364be8b07c2e
KENTARO OKUDA <lonesometraveler@mac.com>
2019-12-18 09:51:23 -0500

    refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
-//! # LCD bitmap 
-//! 
+//! # LCD bitmap
+//!
 //! Usage: cargo run path_to_image_folder name_of_array > filename_to_be_saved.h
-//! Example: cargo run font_13px font_13 > font.h
+//! Example: cargo run font_13px font_s > font_s.h
 
-use std::path::Path;
 use image::Luma;
+use std::path::Path;
 
 pub struct FontToBytes {
     folder: String,
@@ -13,9 +13,7 @@ pub struct FontToBytes {
 }
 
 impl FontToBytes {
-
     pub fn new(mut args: std::env::Args) -> Result<FontToBytes, &'static str> {
-
         args.next(); // skip the first argument which is the name of the program
 
         let folder = match args.next() {
@@ -31,71 +29,94 @@ impl FontToBytes {
         let path = Path::new(&folder);
         let paths = std::fs::read_dir(&path).unwrap();
 
-        // let mut files = paths.filter_map(|entry| {
-        //     entry.ok().and_then(|e|
-        //         e.path().to_str().map(|s| String::from(s))
-        //     )
-        // }).collect::<Vec<String>>();
-    
-        // help: remove closure as shown: `String::from`
-        // https://rust-lang.github.io/rust-clippy/master/index.html#redundant_closure
-        let mut files =
-            paths.filter_map(|entry| {
-            entry.ok().and_then(|e|
-                e.path().file_name()
-                .and_then(|n| n.to_str().map(|s| String::from(s)))
-            )
-            }).collect::<Vec<String>>();
-
+        let mut files = paths
+            .filter_map(|entry| {
+                entry.ok().and_then(|e| {
+                    e.path()
+                        .file_name()
+                        .and_then(|n| n.to_str().map(String::from))
+                })
+            })
+            .collect::<Vec<String>>();
+        
         files.sort();
 
-        Ok(FontToBytes { folder, files, array_name })
+        Ok(FontToBytes {
+            folder,
+            files,
+            array_name,
+        })
+    }
+
+    pub fn run(&self) -> String {
+        let path = format!("{}/{}", self.folder, self.files[0]);
+        let (width, height) = image::open(path).unwrap().to_luma().dimensions();
+
+        let mut output = self.print_macro(width, height);
+
+        for file in self.files.iter() {
+            let path = format!("{}/{}", self.folder, file);
+            let arr = match print_array(Path::new(&path)) {
+                Ok(f) => f,
+                _ => String::from(""),
+            };
+            output += &arr;
+        }
+
+        format!("{}\n}};\n\n#endif", output)
+    }
+
+    pub fn print_macro(&self, width: u32, height: u32) -> String {
+        format!(
+            "#ifndef {}_H_
+#define {}_H_\n
+#define {}_IDX_CNT          ({}u)
+#define {}_WIDTH_BYTES      ({}u)
+#define {}_HEIGHT_ROWS      ({}u)
+#define {}_BYTES_PER_CHAR   ({}_HEIGHT_ROWS * {}_WIDTH_BYTES)\n
+static const unsigned char {}[{}_IDX_CNT][{}_BYTES_PER_CHAR] = {{",
+            self.array_name.to_uppercase(),
+            self.array_name.to_uppercase(),
+            self.array_name.to_uppercase(),
+            self.files.len(),
+            self.array_name.to_uppercase(),
+            width / 8,
+            self.array_name.to_uppercase(),
+            height,
+            self.array_name.to_uppercase(),
+            self.array_name.to_uppercase(),
+            self.array_name.to_uppercase(),
+            self.array_name,
+            self.array_name.to_uppercase(),
+            self.array_name.to_uppercase(),
+        )
     }
 }
 
-pub fn run(generator: FontToBytes) -> String {
-    // let mut output = String::new();
-    let mut output = format!("static const unsigned char {}[][] = {{", generator.array_name); // TODO: return image size and insert array size. add this line at the end with format!
-
-    for file in generator.files {
-        let path = format!("{}/{}", generator.folder, file);
-        // let arr = match print_array(Path::new(&file)) {
-        let arr = match print_array(Path::new(&path)) {
-            Ok(f) => f,
-            _ => String::from(""),
-        };
-        output = format!("{}{}", output, arr);
-    }
-    output = format!("{}\r}};", output);
-
-    output
-}
-
-fn print_array(path: &Path) -> Result<String, &'static str> { // TODO: error handling
-
+fn print_array(path: &Path) -> Result<String, &'static str> {
     let img = image::open(path).unwrap().to_luma();
-
-    // let (width, height) = img.dimensions();
 
     let mut byte: u8 = 0;
 
-    let mut output = format!("\r\n{{ // {} \r\n", path.to_str().unwrap());
+    let mut output = format!("\n\t{{ // {}", path.to_str().unwrap());
 
-    for (bit_counter, pixel) in img.pixels().enumerate() {
-
+    for (bit, pixel) in img.pixels().enumerate() {
         match pixel {
             Luma([0]) => byte &= 0xFE,
             _ => byte |= 0x01,
         }
 
-        if bit_counter % 8 == 7 {
+        if bit % 8 == 7 {
             output = format!("{}0x{:02x},", output, byte);
             byte = 0;
+        }
+
+        if bit % (12 * 8) == 0 {
+            output.push_str("\n\t\t")
         }
 
         byte = byte.rotate_left(1);
     }
 
-    output = format!("{}\r\n}},", output);
-    Ok(output)
+    Ok(format!("{}\n\t}},", output))
 }
--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,10 @@ use std::process;
 use font2bytes::FontToBytes;
 
 fn main() {
-
-    let generator = FontToBytes::new(env::args()).unwrap_or_else( |err| {
+    let generator = FontToBytes::new(env::args()).unwrap_or_else(|err| {
         eprintln!("problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    let output = font2bytes::run(generator);
-    println!("{}", output);
+    println!("{}", generator.run());
 }
